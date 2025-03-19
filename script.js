abdd8635e85755aace84a2bdb09ec7297344d70c
loviaknh <loviaknh@gmail.com>
2025-03-19 12:17:19 +0100

    initial commit

---
+++ script.js
@@ -0,0 +1,21 @@
+/*
+ * ---------------------------------------------------------
+ * Remarque : Intégration du code JavaScript directement dans
+ * les pages concernées plutôt que dans un fichier JS unique.
+ *
+ * Raison : 
+ * 1. **Clarté** : Cela permet de garder le code lié à la logique
+ *    spécifique de chaque page, facilitant ainsi la compréhension
+ *    et la maintenance.
+ * 2. **Performance** : En chargeant uniquement le code nécessaire
+ *    pour chaque page, nous réduisons le poids global des
+ *    ressources chargées, ce qui peut améliorer les temps de
+ *    chargement.
+ * 3. **Modularité** : Chaque page peut évoluer indépendamment,
+ *    ce qui permet d'ajouter ou de modifier des fonctionnalités
+ *    sans affecter l'ensemble du site.
+ *
+ * En résumé, cette approche favorise une meilleure organisation
+ * et une gestion plus efficace du code.
+ * ---------------------------------------------------------
+ */
